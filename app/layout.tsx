d9cf7c7f5f8591ff8649545ddeea40176e00366e
Evgeni <e@shavrin.su>
2023-01-28 13:08:07 +0300

    bg body

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="bg-gradient-to-b from-indigo-900 via-blue-900 to-indigo-800 min-h-screen">{children}</body>
     </html>
   )
 }
